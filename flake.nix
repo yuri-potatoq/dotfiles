a8b88aac7511022e0a5492bba0f7591ffc9271e8
yuri-potatoq <yuri.ylr@outlook.com>
2021-08-06 11:42:56 -0300

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 {
-    description = "My Nix Entrypoint";
+    description = "My little potato flake";
 
     inputs = {
         home-manager.url = "github:nix-community/home-manager";
     };
 
-    outputs = { self, home-manager, nur, nixpkgs, ... }@inputs: {
+    outputs = { self, nixpkgs, home-manager }: {
+
         nixosConfigurations.desktop = nixpkgs.lib.nixosSystem rec {
             system = "x86_64-linux";
             modules = [
-                ./home.nix
-            ];        
-        };
-  };
+                ./pkgs/home.nix
 
+                home-manager.nixosModules.home-manager
+            ];
+        };
+        packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+        defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+    };
 }
