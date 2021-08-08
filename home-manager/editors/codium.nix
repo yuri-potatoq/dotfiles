e3de63adcc17bf8653f8fb942ce990f9a5cb2c5b
yuri-potatoq <yuri.ylr@outlook.com>
2021-08-08 10:47:53 -0300

    fix home.nix

---
+++ home-manager/editors/codium.nix
@@ -0,0 +1,63 @@
+{pkgs, ...}:
+
+{
+  programs = {
+    vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+      userSettings = {
+        # auto update tags when edited
+        "editor.linkedEditing" = true;
+        "editor.rulers" = [ 72 79 ];
+        "editor.formatOnSave" = true;
+        "editor.renderWhitespace" = true;
+        "editor.renderControlCharacters" = true;
+
+        "files.autoSave" = "afterDelay";
+        "files.autoSaveDelay" = 0;
+        
+        "workbench.colorTheme" = "GitHub Dark";
+        "workbench.iconTheme" = "material-icon-theme";
+
+        "window.titleBarStyle" = "custom";
+        "window.zoomLevel" = 0;
+
+        "terminal.integrated.tabs.enabled" = true;
+
+        "[html]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+
+        "nix.enableLanguageServer" = true;
+      };
+      extensions = with pkgs.vscode-extensions; [
+        # Theme
+        github.github-vscode-theme
+
+        # Icons
+        pkief.material-icon-theme
+
+        # Nix
+        jnoortheen.nix-ide
+
+        # Go
+        golang.go
+
+        # Python
+        ms-python.python
+
+        # Rust
+        matklad.rust-analyzer
+
+        # Markdown
+        foam.foam-vscode
+        svsool.markdown-memo
+        yzhang.markdown-all-in-one
+
+        # Misc
+        eamodio.gitlens
+        esbenp.prettier-vscode
+      ];
+    };
+  };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+
+  imports = [
+      ./editors/codium.nix
+      # ./editos/vim.nix
+
+      # TODO: ....
+  ];
+  # home-manager.useUserPackages = true;
+  # home-manager.useGlobalPkgs = true;
+  # home-manager.users.potatoq = {
+    
+
+  #   programs.home-manager.enable = true;
+
+  #   home.username = "potatoq";
+  #   home.homeDirectory = "/home/potatoq"; 
+
+  #   home.stateVersion = "21.11";
+  # };
+}
